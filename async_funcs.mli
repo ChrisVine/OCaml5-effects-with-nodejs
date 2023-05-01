da8cdd33f2410baf73f2cf764dd0b6fbe8c88c5b
Chris Vine <vine.chris@gmail.com>
2023-05-01 14:21:22 +0100

    Adjust sigature of await_sleep function

--- async_funcs.mli
+++ async_funcs.mli
@@ -1,4 +1,4 @@
-val await_sleep : (unit -> [> `Unit ]) -> ([> `Unit ] -> 'a) -> float -> unit
+val await_sleep : (unit -> [> `Unit ]) -> ([> `Unit ] -> unit) -> float -> unit
 
 val await_http_get : (unit -> [> `Get of (string, string) Result.t ]) ->
                      ([> `Get of (string, string) Result.t ] -> unit) ->
